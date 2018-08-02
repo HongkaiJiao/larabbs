cbe7a81148e38ceb62c02a6691d80891c2bd7754
kevinjiao <kevin.jiao@youyoutingche.com>
2018-08-02 07:40:43 +0000

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            Design by <a href="https://github.com/HongkaiJiao">KevinJiao</a> <span style="color:#e27575;font-size: 14px;">❤</span>
+        </p>
+        <p class="pull-right"><a href="mailto:273912572@qq.com">Contact us</a></p>
+    </div>
+</footer>
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('tilte','首页')
+@section('content')
+    <h1>这里是首页</h1>
+@stop
