8de00ec5258bace8065d04b002bc9b882b631277
KevinJiao <basketballjhk@163.com>
2018-11-12 09:31:26 +0000

    User can create reply

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -12,10 +12,17 @@ class ReplyObserver
     public function creating(Reply $reply)
     {
         //
+        $reply->content = clean($reply->content,'user_topic_body');
     }
 
     public function updating(Reply $reply)
     {
         //
+    }
+
+    // 当 Elequont 模型数据成功创建时，created 方法将会被调用
+    public function created(Reply $reply)
+    {
+        $reply->topic->increment('reply_count',1);
     }
 }
